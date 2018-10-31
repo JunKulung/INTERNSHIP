7499b503944bbea029100ff70247f53a0afda054
JunKulung <41711587+JunKulung@users.noreply.github.com>
2018-10-31 11:28:35 +1100

    UpdatedSomeHtml

---
+++ main.js
@@ -0,0 +1,73 @@
+
+    
+         $(document).ready(function() {
+            $(window).scroll(function () {
+                if ($(this).scrollTop() > 50) {
+                $("header a").css("line-height","40px");
+                $(".logo").css("padding-top","5px");
+         
+            } else {
+                $(" header a " ).css("line-height","35px");
+                $(".logo").css("padding-top","0");
+                }
+         })
+         })
+
+
+
+    
+         $(document).ready(function() {
+{
+         
+                $(".index-bg h1").css("opacity","1");
+             
+                    $(".index-bg p").css("opacity","1");
+                    $(".btn").css("opacity","1");
+                    $("header").css("opacity","1");
+            }
+              
+            
+         })
+  
+    
+         $(document).ready(function() {
+            $(window).scroll(function () {
+                if ($(this).scrollTop() > 350) {
+                $(".index-bg").css("opacity","0");
+   
+         
+            } else {
+                $(".index-bg").css("opacity","1");
+            ;
+                }
+         })
+         })
+         $(document).ready(function() {
+            $(window).scroll(function () {
+                if ($(this).scrollTop() > 550) {
+                $(".firstcontent").css("opacity","0");
+   
+         
+            } else {
+                $(".firstcontent").css("opacity","1");
+            ;
+                }
+         })
+         })
+         $(document).ready(function() {
+            $(window).scroll(function () {
+                if ($(this).scrollTop() > 990) {
+                $(".secondcontent").css("opacity","0");
+
+         
+            } else {
+                $(".secondcontent").css("opacity","1");
+            ;
+                }
+         })
+         })
+      
+
+
+
+    
